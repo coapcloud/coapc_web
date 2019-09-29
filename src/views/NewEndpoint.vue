03c68a290ec51d173fe57e3c1136778feb5a1b7f
Jonathan Beri <jmberi@gmail.com>
2019-09-29 12:04:00 -0700

    Save source code; redirect to `/` after saving new endpoint

--- src/views/NewEndpoint.vue
+++ src/views/NewEndpoint.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="12">
         <v-row align="center" justify="center">
-          <v-card min-width="350px">
+          <v-card min-width="480px">
             <v-toolbar color="primary" dark flat>
               <v-toolbar-title>Add new endpoint</v-toolbar-title>
             </v-toolbar>
@@ -12,12 +12,13 @@
                 <v-text-field v-model="subdomain" label="Subdomain" required></v-text-field>
                 <v-text-field v-model="func" label="Function" required></v-text-field>
                 <v-text-field v-model="path" label="Path" required></v-text-field>
+                <v-textarea v-model="source" label="Source Code" required></v-textarea>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn @click="addNewEndpoint" color="primary">submit</v-btn>
-              <!-- <v-btn @click="clear">clear</v-btn> -->
+              <v-btn @click="clear">clear</v-btn>
             </v-card-actions>
           </v-card>
         </v-row>
@@ -27,6 +28,7 @@
 </template>
 <script>
 import { db } from "@/plugins/firebase/db";
+import router from "@/router.js";
 
 export default {
   data: function() {
@@ -42,12 +44,14 @@ export default {
         subdomain: this.subdomain,
         function: this.func,
         path: this.path,
-        owner: localStorage.uid
+        owner: localStorage.uid,
+        source: this.source
       });
+      router.push("/");
+    },
+    reset() {
+      this.$refs.form.reset();
     }
-    // reset() {
-    //   this.$refs.form.reset();
-    // }
   },
   computed: {
     authenticated() {
