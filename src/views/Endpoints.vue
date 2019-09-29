feb441ee451e2afa9962a2c0055760662507561a
Jonathan Beri <jmberi@gmail.com>
2019-09-29 12:58:40 -0700

    update new grid system; display source from db

--- src/views/Endpoints.vue
+++ src/views/Endpoints.vue
@@ -1,15 +1,25 @@
 <template>
   <v-container fluid fill-height v-if="authenticated">
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <h1>My Endpoints</h1>
-        <ul>
-          <li v-for="endpoint in endpoints" v-bind:key="endpoint.id">
-            <div>Subdomain: {{endpoint.subdomain}}, Function: {{endpoint.function}}, Path: {{endpoint.path}}</div>
-          </li>
-        </ul>
-      </v-flex>
-    </v-layout>
+    <v-row>
+      <v-col cols="12">
+        <v-row align="center" justify="center">
+          <v-list min-width="480px">
+            <v-subheader>My Endpoints</v-subheader>
+            <v-list-item-group v-model="item">
+              <v-list-item three-line v-for="endpoint in endpoints" v-bind:key="endpoint.id">
+                <v-list-item-content>
+                  <v-list-item-title>{{endpoint.path}}</v-list-item-title>
+                  <v-list-item-subtitle>{{endpoint.subdomain}}.coap.cloud</v-list-item-subtitle>
+                  <v-list-item-subtitle v-if="endpoint.source">
+                    <code>{{endpoint.source}}</code>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
