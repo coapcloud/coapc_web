58828c7f96cc2cf76ee393aff943114cdd1eba99
Jonathan Beri <jmberi@gmail.com>
2019-09-29 09:03:49 -0700

    Update to 2.0 grid system; prettify

--- src/views/NewEndpoint.vue
+++ src/views/NewEndpoint.vue
@@ -1,24 +1,28 @@
 <template>
   <v-container fluid fill-height v-if="authenticated">
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <form v-on:submit.prevent>
-          <div>
-            <label for="subdomain">Subdomain:</label>
-            <input v-model="subdomain" placeholder="jberi" id="subdomain" name="subdomain" />
-          </div>
-          <div>
-            <label for="function">Function:</label>
-            <input v-model="funct" placeholder="12345" id="function" name="function" />
-          </div>
-          <div>
-            <label for="path">Path:</label>
-            <input v-model="path" placeholder="/foo" id="path" name="path" />
-          </div>
-          <button @click="addNewEndpoint()">Add</button>
-        </form>
-      </v-flex>
-    </v-layout>
+    <v-row>
+      <v-col cols="12">
+        <v-row align="center" justify="center">
+          <v-card min-width="350px">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Add new endpoint</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field v-model="subdomain" label="Subdomain" required></v-text-field>
+                <v-text-field v-model="func" label="Function" required></v-text-field>
+                <v-text-field v-model="path" label="Path" required></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="addNewEndpoint" color="primary">submit</v-btn>
+              <!-- <v-btn @click="clear">clear</v-btn> -->
+            </v-card-actions>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
@@ -28,7 +32,7 @@ export default {
   data: function() {
     return {
       subdomain: "",
-      funct: "",
+      func: "",
       path: ""
     };
   },
@@ -36,11 +40,14 @@ export default {
     addNewEndpoint: function() {
       db.collection("endpoints").add({
         subdomain: this.subdomain,
-        function: this.funct,
+        function: this.func,
         path: this.path,
         owner: localStorage.uid
       });
     }
+    // reset() {
+    //   this.$refs.form.reset();
+    // }
   },
   computed: {
     authenticated() {
