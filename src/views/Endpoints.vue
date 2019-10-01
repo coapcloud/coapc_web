6bccc49a443c55981f4de33e26ebeba773bbd61f
Jonathan Beri <jmberi@gmail.com>
2019-09-30 18:04:07 -0700

    More vuetify cleanup for the endpoints list

--- src/views/Endpoints.vue
+++ src/views/Endpoints.vue
@@ -1,15 +1,17 @@
 <template>
   <v-container fluid fill-height v-if="authenticated">
     <v-row>
-      <v-col cols="12">
+      <v-col>
         <v-row align="center" justify="center">
           <v-list min-width="480px">
             <v-subheader>My Endpoints</v-subheader>
-            <v-list-item-group v-model="item">
-              <v-list-item three-line v-for="endpoint in endpoints" v-bind:key="endpoint.id">
+            <v-list-item-group>
+              <v-list-item two-line v-for="endpoint in endpoints" v-bind:key="endpoint.id">
                 <v-list-item-content>
-                  <v-list-item-title>{{endpoint.path}}</v-list-item-title>
-                  <v-list-item-subtitle>{{endpoint.subdomain}}.coap.cloud</v-list-item-subtitle>
+                  <v-list-item-title>
+                    <span class="font-weight-thin">{{endpoint.subdomain + ".coap.cloud"}}</span>
+                    {{endpoint.path}}
+                  </v-list-item-title>
                   <v-list-item-subtitle v-if="endpoint.source">
                     <code>{{endpoint.source}}</code>
                   </v-list-item-subtitle>
